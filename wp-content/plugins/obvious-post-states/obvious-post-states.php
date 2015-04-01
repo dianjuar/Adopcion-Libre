70c67a9985a543be6a502fa7d00e75b5e7fde619
dianjuar <dianjuar@gmail.com>
2015-04-01 03:29:47 -0430

    Mejora usabilidad. Plugin para poder visualizar mejor los estados de los post

---
+++ wp-content/plugins/obvious-post-states/obvious-post-states.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Plugin Name: Obvious Post States
+Plugin URI: http://sevenbold.com/wordpress/
+Description: Make your WordPress post state text (draft, pending, sticky, etc) stand out.
+Author: Ryan Sommers
+Version: 1.0.2
+Author URI: http://ryansommers.com
+*/
+
+function obvious_post_states_files() {
+  wp_enqueue_style( 'obvious-post-states', plugins_url('obvious-post-states.css', __FILE__) );
+  wp_enqueue_script( 'obvious-post-states', plugins_url( "js/obvious-post-states.js", __FILE__ ), array( 'jquery' ), '1.0.0', true );
+}
+add_action( 'admin_enqueue_scripts', 'obvious_post_states_files' );
+add_action( 'login_enqueue_scripts', 'obvious_post_states_files' );
+
+// Remove the hyphen before the post state
+add_filter( 'display_post_states', 'obvious_post_states_post_state' );
+function obvious_post_states_post_state( $post_states ) {
+	if ( !empty($post_states) ) {
+		$state_count = count($post_states);
+		$i = 0;
+		foreach ( $post_states as $state ) {
+			++$i;
+			( $i == $state_count ) ? $sep = '' : $sep = '';
+			echo "<span class='post-state'>$state$sep</span>";
+		}
+	}
+}
+
+?>
