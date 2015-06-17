2f9ae642cc4fa77fe2c8b3a2da41752f16263ef4
Yossely Mendoza <yossely7@gmail.com>
2015-06-16 20:08:26 -0430

    Removida la opción de edición rápida para todos los usuarios menos para al admin (rol admin que trae wordpress por defecto)

--- wp-content/themes/theme-servicioComunitario/Funcionalidades/ListarEditarUsuarios.php
+++ wp-content/themes/theme-servicioComunitario/Funcionalidades/ListarEditarUsuarios.php
@@ -126,4 +126,16 @@ function list_moderador_suscriptor($user_search) {
 
 }
 
+
+/*REMOVER EDICION RAPIDA*/
+
+function remove_quick_edit( $actions ) {
+	unset($actions['inline hide-if-no-js']);
+	return $actions;
+}
+
+if ( !current_user_can( 'manage_options' ))
+	add_filter('post_row_actions','remove_quick_edit',10,1);
+
+
 ?>
