92a5df05a5afee3f73a55446dfc37dbc4424f6dc
karilaz <anirak.laz@gmail.com>
2015-06-26 22:40:52 -0430

    mas estilos en admin

--- wp-content/themes/theme-servicioComunitario/functions.php
+++ wp-content/themes/theme-servicioComunitario/functions.php
@@ -1,13 +1,12 @@
 <?php
-
-	//Funcion para menus
+	/*===== SITIO Funcion para mostar menus ON ==========================================*/
 	register_nav_menus(
     	array(
         	'menu-index'=>'Menu home',
         	'menu-top'=>'Menu Top',
         ));
-
-	//funcion para widget
+	/*===== SITIO Funcion para mostar menus OFF =========================================*/
+	/*===== SITIO Funcion para mostar widgets ON ========================================*/
 	if (function_exists('register_sidebar')) {
 		register_sidebar(array(
 		'name'=> 'Inicio sesion',
@@ -27,7 +26,8 @@
 		'after_title' => '</h2>',
 		));
 	}
-	// Campos personalizados
+	/*===== SITIO Funcion para mostar widgets OFF =======================================*/
+	/*===== ADMIN Declarar meta box - Post ON ===========================================*/
 	$key = "post";
 	$meta_boxes = array(
 		"tipo" => array(
@@ -55,8 +55,8 @@
     		"titulo" => "Dirección:",
     		"descripcion" => "Dirección actual de la mascota"),
 	);
-	 
-	//Crear campos personalizados
+	/*===== ADMIN Declarar meta box - Post OFF ==========================================*/
+	/*===== ADMIN Crear meta box - Post ON ==============================================*/
 	function crear_meta_box() {
 	   global $key;
 	 
@@ -64,8 +64,9 @@
 	       add_meta_box( 'nuevo-meta-boxes', ucfirst( $key ) . ' Datos:', 'mostrar_meta_box', 'post', 'normal', 'high' );
 	   }
 	}
-
-	//Mostrar campos de la entrada
+	add_action( 'admin_menu', 'crear_meta_box' );
+	/*===== ADMIN Crear meta box - Post OFF =============================================*/
+	/*===== ADMIN Mostar meta box - Post ON =============================================*/
 	function mostrar_meta_box() {
 		global $post, $meta_boxes, $key;
 		global $current_user;
@@ -126,8 +127,8 @@
 		</div>
 		<?php
 	}
-
-	//Guardar datos de la entrada
+	/*===== ADMIN Mostar meta box - Post OFF=============================================*/
+	/*===== ADMIN Grabar meta box - Post ON =============================================*/
 	function grabar_meta_box( $post_id ) {
 	    global $post, $meta_boxes, $key, $aux;
 	 
@@ -144,8 +145,9 @@
 	 		update_post_meta( $post_id, $key, $data );
 	    
 	}
- 	
- 	//Titulos de la entrada
+	add_action( 'save_post', 'grabar_meta_box' );
+ 	/*===== ADMIN Grabar meta box - Post OFF ============================================*/
+ 	/*===== ADMIN Editar tabla - Post OFF ===============================================*/
 	function editar_titulos_columnas($columns){
 			$columns = array(
 				"cb" => "<input type=\"checkbox\" />",
@@ -158,8 +160,9 @@
 			);
 			return $columns;
 	}
-	
-	//Mostrar valores de la entrada
+	add_filter("manage_edit-post_columns", "editar_titulos_columnas");
+	/*===== ADMIN Editar tabla - Post OFF ===============================================*/
+	/*===== ADMIN Mostrar meta box en tabla - Post ON ===================================*/
 	function obt_valores_columnas($column){
     	global $post, $meta_boxes, $key;
  
@@ -185,16 +188,9 @@
     	                break;
     	}
 	}
-
-	//Eventos action y filter
-	add_action( 'admin_menu', 'crear_meta_box' );
-	add_action( 'save_post', 'grabar_meta_box' );
-	add_filter("manage_edit-post_columns", "editar_titulos_columnas");
 	add_action("manage_posts_custom_column",  "obt_valores_columnas");
-	add_filter( 'the_search_query', 'searchAll' );
-
-
-	//Set a custom role for a new user
+	/*===== ADMIN Mostrar meta box en tabla - Post OFF ==================================*/
+	/*===== Set a custom role for a new user ON =========================================*/
 	function oa_social_login_set_new_user_role ($user_role)
 	{
 	  //This is an example for a custom setting with one role
@@ -209,10 +205,11 @@
 	  //The new user will be created with this role
 	  return $user_role;
 	}
-	 
-	//This filter is applied to the roles of new users
+	/*===== Set a custom role for a new user OFF ========================================*/
+	/*===== This filter is applied to the roles of new users ON =========================*/
 	add_filter('oa_social_login_filter_new_user_role', 'oa_social_login_set_new_user_role');
-
+	/*===== This filter is applied to the roles of new users OFF ========================*/
+	/*===== ADMIN Quitar link a la barra superior ON ====================================*/
 	function remove_admin_bar_links() {
 		global $wp_admin_bar;
 		$wp_admin_bar->remove_menu('wp-logo');
@@ -222,7 +219,8 @@
 		$wp_admin_bar->remove_menu('new-content');
 	}
 	add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
-
+	/*===== ADMIN Quitar link a la barra superior OFF ===================================*/
+	/*===== ADMIN Agregar link a la barra superior ON ===================================*/
 	function add_sumtips_admin_bar_link() {
 		global $wp_admin_bar;
 		$wp_admin_bar->add_menu( array(
@@ -232,8 +230,8 @@
 		) );
 	}
 	add_action('admin_bar_menu', 'add_sumtips_admin_bar_link',25);
-
-	/* Agregar hoja de estilo */
+	/*===== ADMIN Agregar link a la barra superior OFF ==================================*/
+	/*===== ADMIN Agregar hoja CSS ON ===================================================*/
 	function wb_admin_css() {
 		$url = content_url('/themes/theme-servicioComunitario/css/wp-admin.css', __FILE__);
 	    //$url = get_settings('siteurl') . '/wp-content/plugins/wp-admin-theme/wp-admin.css';
@@ -243,39 +241,46 @@
 	    ';
 	}
 	add_action('admin_head','wb_admin_css', 1000);
-
+	/*===== ADMIN Agregar hoja CSS OFF ==================================================*/
+	/*===== ADMIN esconder opciones personales - perfil ON ==============================*/
 	function hide_personal_options(){
 	?>
 	<script type="text/javascript">
 	  jQuery(document).ready(function(){
-	    jQuery("#your-profile .form-table:first, #your-profile h3:first, #contextual-help-link-wrap").remove();
+	    jQuery("#your-profile .form-table:first, #your-profile h3:first").remove();
 	  });
 	</script>
 	<?php
 	}
 	add_action('admin_head','hide_personal_options');
-
-
-	//Deshabilita la funcion de autoguardado de los post
+	/*===== ADMIN esconder opciones personales - perfil OFF =============================*/
+	/*===== ADMIN Deshabilita la funcion de autoguardado de los post ON =================*/
 	function disableAutoSave(){
 		wp_deregister_script('autosave');
 	}
 	add_action( 'wp_print_scripts', 'disableAutoSave' );
-
-	//Login estilos
-
+	/*===== ADMIN Deshabilita la funcion de autoguardado de los post ON =================*/
+	/*===== LOGIN-FORM Agregar hoja CSS ON ==============================================*/
 	function my_login_stylesheet() {
 	    wp_enqueue_style( 'custom-login', content_url('/themes/theme-servicioComunitario/css/wp-login.css', __FILE__) );
 	    wp_enqueue_script( 'custom-login', get_template_directory_uri() . '/style-login.js' );
 	}
 	add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
-
-	/* Esconder opciones de pantalla */
+	/*===== LOGIN-FORM Agregar hoja CSS OFF =============================================*/
+	/*===== ADMIN esconder opciones de pantalla ON ======================================*/
 	function remove_screen_options(){
 	    return false;
 	}
 	add_filter('screen_options_show_screen', 'remove_screen_options');
-
+	/*===== ADMIN esconder opciones de pantalla OFF =====================================*/
+	/*===== ADMIN esconder ayuda ON =====================================================*/
+	add_action('admin_head', 'mytheme_remove_help_tabs');
+	function mytheme_remove_help_tabs() {
+	    $screen = get_current_screen();
+	    $screen->remove_help_tabs();
+	}
+	/*===== ADMIN esconder ayuda OFF ====================================================*/
+	/*===== ADMIN esconder widgets - escritorio ON ======================================*/
 	function remove_dashboard_widgets() {
 
 		global $wp_meta_boxes;
@@ -284,6 +289,7 @@
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);
 
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);
@@ -291,8 +297,8 @@
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 	}
 	add_action('wp_dashboard_setup', 'remove_dashboard_widgets');
-
-	//-- Titulo Inicio Sesion ------
+	/*===== ADMIN esconder widgets - escritorio OFF =====================================*/
+	/*===== LOGIN-FORM Titulo Inicio Sesion ON ==========================================*/
 	function titulo_inicio_sesion() { ?>
 	    <script type="text/javascript">
 		  jQuery(document).ready(function(){
@@ -302,8 +308,8 @@
 		<?php
 	}
 	add_action( 'login_form', 'titulo_inicio_sesion' );
-	//-- Titulo Inicio Sesion ------
-	//-- Titulo Registro ------
+	/*===== LOGIN-FORM Titulo Inicio Sesion OFF =========================================*/
+	/*===== LOGIN-FORM Titulo Registro ON ===============================================*/
 	function titulo_registro() { ?>
 	    <script type="text/javascript">
 		  jQuery(document).ready(function(){
@@ -313,8 +319,8 @@
 		<?php
 	}
 	add_action( 'register_form', 'titulo_registro' );
-	//-- Titulo Registro ------
-	//-- Titulo Olvido su contraseña ------
+	/*===== LOGIN-FORM Titulo Registro OFF ==============================================*/
+	/*===== LOGIN-FORM Titulo Olvido su contraseña ON ===================================*/
 	function titulo_contrasena() { ?>
 	    <script type="text/javascript">
 		  jQuery(document).ready(function(){
@@ -324,8 +330,8 @@
 		<?php
 	}
 	add_action( 'lostpassword_form', 'titulo_contrasena' );
-	//-- Titulo Olvido su contraseña ------
-	//-- Titulo Olvido su contraseña ------
+	/*===== LOGIN-FORM Titulo Olvido su contraseña OFF ==================================*/
+	/*===== LOGIN-FORM Titulo reset contraseña ON =======================================*/
 	function titulo_reset() { ?>
 	    <script type="text/javascript">
 		  jQuery(document).ready(function(){
@@ -335,21 +341,78 @@
 		<?php
 	}
 	add_action( 'resetpass_form', 'titulo_reset' );
-	//-- Titulo Olvido su contraseña ------
-	//-- Login Footer ------
+	/*===== LOGIN-FORM Titulo reset contraseña OFF ======================================*/
+	/*===== LOGIN-FORM Y ADMIN agregar footer ON ========================================*/
 	function showFooter() {
 		get_footer();
 	}
 	add_action( 'login_footer', 'showFooter' );
 	add_action( 'admin_footer', 'showFooter' );
-	//-- Login Footer ------
-	//-- Titulo Olvido su contraseña ------
+	/*===== LOGIN-FORM Y ADMIN agregar footer OFF =======================================*/
+	/*===== LOGIN-FORM Y ADMIN agregar header ON ========================================*/
 	function showHeader() {
 		 get_header();
 	}
 	add_action( 'login_head', 'showHeader' );
 	add_action( 'admin_head', 'showHeader' );
-	//-- Titulo Olvido su contraseña ------
+	/*===== LOGIN-FORM Y ADMIN agregar header OFF =======================================*/
+	/*===== ADMIN Bienvanida - Escritorio ON ============================================*/
+	function nuevos_widgets_escritorio() {
+		wp_add_dashboard_widget( 'tutorial_bienvenido_escritorio', 'Bienvenido a la sección de administración', 'escritorio_bienvenida' );
+	}
+	function escritorio_bienvenida(){ ?>
+		<p>En esta sección se compone de:</p>
+		<ul>
+		<li><strong>Mascotas</strong> - Alli podras crear mascotas para darlas en adopcion, para reportarlas como encontradas o perdidas</li>
+		<li><strong>Comentarios</strong> - Alli podras ver todos los comentarios que han hecho en tus entradas</li>
+		<li><strong>Perfil</strong> - Alli podras cambiar los datos de tu perfil</li>
+		</ul>
+		
+	<?php }
+	add_action( 'wp_dashboard_setup', 'nuevos_widgets_escritorio' );
+	/*===== ADMIN Bienvanida - Escritorio OFF ===========================================*/
+	/*===== ADMIN Titulos descripcion - Post ON =========================================*/
+	function titulo_des() { ?>
+	    <script type="text/javascript">
+		  jQuery(document).ready(function(){
+		    jQuery(".wp-editor-expand #wp-content-editor-tools").html("<span>Introduce una descripción</span>");
+		  });
+		</script>
+		<?php
+	}
+	add_action('edit_form_top', 'titulo_des');
+	/*===== ADMIN Titulos descripcion - Post OFF ========================================*/
+	/*===== ADMIN Cambiar nombre - Post ON ==============================================*/
+	function revcon_change_post_label() {
+	    global $menu;
+	    global $submenu;
+	    $menu[5][0] = 'Mascotas';
+	    $submenu['edit.php'][5][0] = 'Mascotas';
+	    $submenu['edit.php'][10][0] = 'Nueva mascotas';
+	    $submenu['edit.php'][16][0] = 'Etiquetas de mascotas';
+	    echo '';
+	}
+	function revcon_change_post_object() {
+	    global $wp_post_types;
+	    $labels = &$wp_post_types['post']->labels;
+	    $labels->name = 'Mascotas';
+	    $labels->singular_name = 'Mascotas';
+	    $labels->add_new = 'Nueva mascota';
+	    $labels->add_new_item = 'Nueva Mascota';
+	    $labels->edit_item = 'Editar Mascotas';
+	    $labels->new_item = 'Mascotas';
+	    $labels->view_item = 'Ver mascota';
+	    $labels->search_items = 'Buscar mascotas';
+	    $labels->not_found = 'No se encontraron mascotas';
+	    $labels->not_found_in_trash = 'No se encontraron en la papelera';
+	    $labels->all_items = 'Todas las Mascotas';
+	    $labels->menu_name = 'Mascotas';
+	    $labels->name_admin_bar = 'Mascotas';
+	}
+	 
+	add_action( 'admin_menu', 'revcon_change_post_label' );
+	add_action( 'init', 'revcon_change_post_object' );
+	/*===== ADMIN Cambiar nombre - Post OFF =============================================*/
 ?>
 
 <?php
