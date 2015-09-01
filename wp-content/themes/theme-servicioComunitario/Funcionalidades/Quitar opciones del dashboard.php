6ba671349c4bccc6930b337b3899e216ca636dc4
dianjuar <dianjuar@gmail.com>
2015-09-01 01:39:09 -0430

    Cuando un usuario proviene de la seccion de animales en adopción al darle click en el botón "dar en adopción" se carga la página para crear una nueva entrada con la categoría "animales en adopción".
    Así para todas las categorías.
    
    Extrañamente el rol Administrador (programador) no tenía las categorias como radiobuttons sino como checkbox. A mi parecer como está construida la página esto no debe ser posible ya que distorciona el sentido de la misma. Esto a sido arreglado.

--- wp-content/themes/theme-servicioComunitario/Funcionalidades/Quitar opciones del dashboard.php
+++ wp-content/themes/theme-servicioComunitario/Funcionalidades/Quitar opciones del dashboard.php
@@ -42,15 +42,42 @@ Author URI: http://untame.net
 
 
 
-    // RadioButton category on post page para (Todos los usuario menos para el Admin-programador)
- 
-     if(!current_user_can( 'activate_plugins' ) && (strstr($_SERVER['REQUEST_URI'], 'wp-admin/post-new.php') || 
+    // RadioButton category on post page para todos los usuarios y es seleccionado la categoria correspondiente dependiendo de dond provenga el usuario
+     if( (strstr($_SERVER['REQUEST_URI'], 'wp-admin/post-new.php') || 
         strstr($_SERVER['REQUEST_URI'], 'wp-admin/post.php'))) {
             ob_start('one_category_only');
      }
 
-     function one_category_only($content) {
-        $content = str_replace('type="checkbox" ', 'type="radio" ', $content);
+     function one_category_only($content) 
+     {
+        $content = str_replace('type="checkbox" ', 'type="radio" disabled ', $content);
+
+
+        switch ( $_GET['post_category'] ) {
+            case 'adopcion':
+                //<input value="2" name="post_category[]" id="in-category-2" type="radio">
+                $content = str_replace('<input value="2" ', 
+                                       '<input value="2" checked ', 
+                                       $content);
+                break;
+
+            case 'encontrado':
+                $content = str_replace('<input value="3" ', 
+                                       '<input value="3" checked ', 
+                                       $content);
+                break;
+
+            case 'perdido':
+                $content = str_replace('<input value="4" ', 
+                                       '<input value="4" checked ', 
+                                       $content);
+                break;
+
+            default:
+                # nothing
+                break;
+        }
+
         return $content;
      }
 
