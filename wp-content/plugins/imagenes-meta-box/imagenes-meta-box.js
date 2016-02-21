38abb01730be63623c409baed4857820b21344d4
chicho <jgpaz19@gmail.com>
2016-02-21 01:26:40 -0430

    iagregacion de imagenes arreglo completo

--- wp-content/plugins/imagenes-meta-box/imagenes-meta-box.js
+++ wp-content/plugins/imagenes-meta-box/imagenes-meta-box.js
@@ -1,26 +1,312 @@
 jQuery(document).ready(function($) {
  
-   var formfield = null;
- 
-   $('.img_boton').click(function() {
-      $('html').addClass('Image');
-      formfield = $(this).prev('.em_mtbx_img').attr('name');
-      tb_show('', 'media-upload.php?type=image&TB_iframe=true');
-      return false;
-    });
- 
-   window.original_send_to_editor = window.send_to_editor;
-   window.send_to_editor = function(html){
-      var fileurl;
- 
-      if (formfield != null) {
-        fileurl = $('img',html).attr('src');
-        $('#'+formfield).val(fileurl);
-        tb_remove();
-        $('html').removeClass('Image');
-        formfield = null;
-       } else {
-        window.original_send_to_editor(html);
-       }
-    };
+var addButton = document.getElementById( 'img_boton1' );
+var deleteButton = document.getElementById( 'image-delete-button1' );
+var hidden = document.getElementById( 'em_mtbx_img1' );
+var img = document.getElementById( 'image-tag1' );
+var srcimages = img.getAttribute("src")
+var customUploader = wp.media({
+    title: 'Seleccione una imagen',
+    button: {
+        text: 'Usar esta imagen'
+    },
+    multiple: false
 });
+
+addButton.addEventListener( 'click', function() {
+    if ( customUploader ) {
+        customUploader.open();
+    }
+} );
+
+deleteButton.addEventListener( 'click', function() {
+    img.removeAttribute( 'src' );
+    img.setAttribute( 'src', srcimages );
+    hidden.removeAttribute( 'value' );
+    toggleVisibility( 'DELETE' );
+} );
+
+
+customUploader.on( 'select', function() {
+    var attachment = customUploader.state().get('selection').first().toJSON();
+    img.setAttribute( 'src', attachment.url );
+    hidden.setAttribute( 'value', attachment.url );
+    toggleVisibility( 'ADD' );
+} );
+
+var toggleVisibility = function( action ) {
+    if ( 'ADD' === action ) {
+        addButton.style.display = 'none';
+        deleteButton.style.display = '';
+        img.setAttribute( 'style', 'width: 200px;');
+        img.setAttribute( 'style', 'height: 200px;');
+    }
+
+    if ( 'DELETE' === action ) {
+        addButton.style.display = '';
+        deleteButton.style.display = 'none';
+        img.removeAttribute('style');
+    }
+};
+
+window.addEventListener( 'DOMContentLoaded', function() {
+    if ( "" === customUploads.imageData || 0 === customUploads.imageData.length ) {
+        toggleVisibility( 'DELETE' );
+    } else {
+        img.setAttribute( 'src', customUploads.imageData.src );
+        hidden.setAttribute( 'value', JSON.stringify([ customUploads.imageData ]) );
+        toggleVisibility( 'ADD' );
+    }
+} );
+
+});
+
+jQuery(document).ready(function($) {
+ 
+var addButton = document.getElementById( 'img_boton2' );
+var deleteButton = document.getElementById( 'image-delete-button2' );
+var hidden = document.getElementById( 'em_mtbx_img2' );
+var img = document.getElementById( 'image-tag2' );
+var srcimages = img.getAttribute("src")
+var customUploader = wp.media({
+    title: 'Seleccione una imagen',
+    button: {
+        text: 'Usar esta imagen'
+    },
+    multiple: false
+});
+
+addButton.addEventListener( 'click', function() {
+    if ( customUploader ) {
+        customUploader.open();
+    }
+} );
+
+deleteButton.addEventListener( 'click', function() {
+    img.removeAttribute( 'src' );
+    img.setAttribute( 'src', srcimages );
+    hidden.removeAttribute( 'value' );
+    toggleVisibility( 'DELETE' );
+} );
+
+
+customUploader.on( 'select', function() {
+    var attachment = customUploader.state().get('selection').first().toJSON();
+    img.setAttribute( 'src', attachment.url );
+    hidden.setAttribute( 'value', attachment.url );
+    toggleVisibility( 'ADD' );
+} );
+
+var toggleVisibility = function( action ) {
+    if ( 'ADD' === action ) {
+        addButton.style.display = 'none';
+        deleteButton.style.display = '';
+        img.setAttribute( 'style', 'width: 200px;');
+        img.setAttribute( 'style', 'height: 200px;');
+    }
+
+    if ( 'DELETE' === action ) {
+        addButton.style.display = '';
+        deleteButton.style.display = 'none';
+        img.removeAttribute('style');
+    }
+};
+
+window.addEventListener( 'DOMContentLoaded', function() {
+    if ( "" === customUploads.imageData || 0 === customUploads.imageData.length ) {
+        toggleVisibility( 'DELETE' );
+    } else {
+        img.setAttribute( 'src', customUploads.imageData.src );
+        hidden.setAttribute( 'value', JSON.stringify([ customUploads.imageData ]) );
+        toggleVisibility( 'ADD' );
+    }
+} ); 
+
+});
+
+jQuery(document).ready(function($) {
+ 
+var addButton = document.getElementById( 'img_boton3' );
+var deleteButton = document.getElementById( 'image-delete-button3' );
+var hidden = document.getElementById( 'em_mtbx_img3' );
+var img = document.getElementById( 'image-tag3' );
+var srcimages = img.getAttribute("src")
+var customUploader = wp.media({
+    title: 'Seleccione una imagen',
+    button: {
+        text: 'Usar esta imagen'
+    },
+    multiple: false
+});
+
+addButton.addEventListener( 'click', function() {
+    if ( customUploader ) {
+        customUploader.open();
+    }
+} );
+
+deleteButton.addEventListener( 'click', function() {
+    img.removeAttribute( 'src' );
+    img.setAttribute( 'src', srcimages );
+    hidden.removeAttribute( 'value' );
+    toggleVisibility( 'DELETE' );
+} );
+
+
+customUploader.on( 'select', function() {
+    var attachment = customUploader.state().get('selection').first().toJSON();
+    img.setAttribute( 'src', attachment.url );
+    hidden.setAttribute( 'value', attachment.url );
+    toggleVisibility( 'ADD' );
+} );
+
+var toggleVisibility = function( action ) {
+    if ( 'ADD' === action ) {
+        addButton.style.display = 'none';
+        deleteButton.style.display = '';
+        img.setAttribute( 'style', 'width: 200px;');
+        img.setAttribute( 'style', 'height: 200px;');
+    }
+
+    if ( 'DELETE' === action ) {
+        addButton.style.display = '';
+        deleteButton.style.display = 'none';
+        img.removeAttribute('style');
+    }
+};
+
+window.addEventListener( 'DOMContentLoaded', function() {
+    if ( "" === customUploads.imageData || 0 === customUploads.imageData.length ) {
+        toggleVisibility( 'DELETE' );
+    } else {
+        img.setAttribute( 'src', customUploads.imageData.src );
+        hidden.setAttribute( 'value', JSON.stringify([ customUploads.imageData ]) );
+        toggleVisibility( 'ADD' );
+    }
+} );
+});
+
+jQuery(document).ready(function($) {
+ 
+var addButton = document.getElementById( 'img_boton4' );
+var deleteButton = document.getElementById( 'image-delete-button4' );
+var hidden = document.getElementById( 'em_mtbx_img4' );
+var img = document.getElementById( 'image-tag4' );
+var srcimages = img.getAttribute("src")
+var customUploader = wp.media({
+    title: 'Seleccione una imagen',
+    button: {
+        text: 'Usar esta imagen'
+    },
+    multiple: false
+});
+
+addButton.addEventListener( 'click', function() {
+    if ( customUploader ) {
+        customUploader.open();
+    }
+} );
+
+deleteButton.addEventListener( 'click', function() {
+    img.removeAttribute( 'src' );
+    img.setAttribute( 'src', srcimages );
+    hidden.removeAttribute( 'value' );
+    toggleVisibility( 'DELETE' );
+} );
+
+
+customUploader.on( 'select', function() {
+    var attachment = customUploader.state().get('selection').first().toJSON();
+    img.setAttribute( 'src', attachment.url );
+    hidden.setAttribute( 'value', attachment.url );
+    toggleVisibility( 'ADD' );
+} );
+
+var toggleVisibility = function( action ) {
+    if ( 'ADD' === action ) {
+        addButton.style.display = 'none';
+        deleteButton.style.display = '';
+        img.setAttribute( 'style', 'width: 200px;');
+        img.setAttribute( 'style', 'height: 200px;');
+    }
+
+    if ( 'DELETE' === action ) {
+        addButton.style.display = '';
+        deleteButton.style.display = 'none';
+        img.removeAttribute('style');
+    }
+};
+
+window.addEventListener( 'DOMContentLoaded', function() {
+    if ( "" === customUploads.imageData || 0 === customUploads.imageData.length ) {
+        toggleVisibility( 'DELETE' );
+    } else {
+        img.setAttribute( 'src', customUploads.imageData.src );
+        hidden.setAttribute( 'value', JSON.stringify([ customUploads.imageData ]) );
+        toggleVisibility( 'ADD' );
+    }
+} );
+});
+
+jQuery(document).ready(function($) {
+ 
+var addButton = document.getElementById( 'img_boton5' );
+var deleteButton = document.getElementById( 'image-delete-button5' );
+var hidden = document.getElementById( 'em_mtbx_img5' );
+var img = document.getElementById( 'image-tag5' );
+var srcimages = img.getAttribute("src")
+var customUploader = wp.media({
+    title: 'Seleccione una imagen',
+    button: {
+        text: 'Usar esta imagen'
+    },
+    multiple: false
+});
+
+addButton.addEventListener( 'click', function() {
+    if ( customUploader ) {
+        customUploader.open();
+    }
+} );
+
+deleteButton.addEventListener( 'click', function() {
+    img.removeAttribute( 'src' );
+    img.setAttribute( 'src', srcimages );
+    hidden.removeAttribute( 'value' );
+    toggleVisibility( 'DELETE' );
+} );
+
+
+customUploader.on( 'select', function() {
+    var attachment = customUploader.state().get('selection').first().toJSON();
+    img.setAttribute( 'src', attachment.url );
+    hidden.setAttribute( 'value', attachment.url );
+    toggleVisibility( 'ADD' );
+} );
+
+var toggleVisibility = function( action ) {
+    if ( 'ADD' === action ) {
+        addButton.style.display = 'none';
+        deleteButton.style.display = '';
+        img.setAttribute( 'style', 'width: 200px;');
+        img.setAttribute( 'style', 'height: 200px;');
+    }
+
+    if ( 'DELETE' === action ) {
+        addButton.style.display = '';
+        deleteButton.style.display = 'none';
+        img.removeAttribute('style');
+    }
+};
+
+window.addEventListener( 'DOMContentLoaded', function() {
+    if ( "" === customUploads.imageData || 0 === customUploads.imageData.length ) {
+        toggleVisibility( 'DELETE' );
+    } else {
+        img.setAttribute( 'src', customUploads.imageData.src );
+        hidden.setAttribute( 'value', JSON.stringify([ customUploads.imageData ]) );
+        toggleVisibility( 'ADD' );
+    }
+} );
+});
+
--- wp-content/plugins/imagenes-meta-box/imagenes-meta-box.php
+++ wp-content/plugins/imagenes-meta-box/imagenes-meta-box.php
@@ -14,8 +14,8 @@ add_action( 'add_meta_boxes', 'em_mtbx_img_crear' );
  
 function em_mtbx_img_crear() {
  
-	// Creamos el meta box personalizado
-	add_meta_box( 
+  // Creamos el meta box personalizado
+  add_meta_box( 
                 'em-img-meta', // atributo ID
                 'Fotos de la mascota:', // Título
                 'em_mtbx_img_function', // Función que muestra el HTML que aparecerá en la pantalla
@@ -23,20 +23,38 @@ function em_mtbx_img_crear() {
                 'normal', // Parte de la pantalla donde aparecerá. Puede ser 'normal', 'advanced', o 'side'
                 'high' // Prioridad u orden en el que aparecerá. Puede ser 'high', 'core', 'default' o 'low'
                 );
-	
+  
 }
 
-function em_mtbx_img_function( $post ) { ?>
+function em_mtbx_img_function( $post ) {
+
+function register_metaboxes() {
+  add_meta_box('image_metabox', 'Image Uploader', __NAMESPACE__ . '\image_uploader_callback');
+}
+
+function register_admin_script() {
+  wp_enqueue_script( 'wp_img_upload', plugin_dir_url( __FILE__ ) . '/image-upload.js', array('jquery', 'media-upload'), '0.0.2', true );
+  wp_localize_script( 'wp_img_upload', 'customUploads', array( 'imageData' => get_post_meta( get_the_ID(), 'custom_image_data', true ) ) );
+}
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\register_admin_script' );
+ ?>
         <strong>Nota:</strong><span>Debe cargar minimo dos (2) fotos de la mascota</span><br><br>
 
         <?php
+        $image = get_template_directory_uri().'/img/no-hay-imagen.jpg'; // Ponemos una imagen por defecto
         $em_mtbx_img1 = get_post_meta( $post->ID, '_em_mtbx_img1', true );
         ?>
         <strong>Foto 1:</strong><br>
-        <input required id="em_mtbx_img1" class="em_mtbx_img" type="text" size="40" name="em_mtbx_img1" value="<?php echo esc_url( $em_mtbx_img1 ); ?>" />
+        <input  required id="em_mtbx_img1"   type="hidden" size="40" name="em_mtbx_img1" value="<?php echo esc_url( $em_mtbx_img1 ); ?>"/>
           <input id="img_boton1" type="button" value="Seleccionar imagen" class="img_boton button-secondary"  />
+          <input type="button" id="image-delete-button1" class="button" value="Borrar Imagen" style="display: none;">
+         
           <br /><br />
-          <?php if($em_mtbx_img1 && $em_mtbx_img1 != '') echo '<img src="' . esc_url( $em_mtbx_img1 ) .'" width="150" height="150" alt="" />'; ?>  
+          <?php  
+          if (esc_url( $em_mtbx_img1 )=="") { ?>
+          <img id="image-tag1" src="<?php echo $image; ?>"/><br />
+          <?php }; ?>
+          <?php if($em_mtbx_img1 && $em_mtbx_img1 != '') echo '<img src="' . esc_url( $em_mtbx_img1 ) .'" width="200" height="200" alt="" />'; ?>
           <div class="clear"></div>
           <br />
           
@@ -44,10 +62,16 @@ function em_mtbx_img_function( $post ) { ?>
         $em_mtbx_img2 = get_post_meta( $post->ID, '_em_mtbx_img2', true );
         ?>
         <strong>Foto 2:</strong><br>
-        <input required id="em_mtbx_img2" class="em_mtbx_img" type="text" size="40" name="em_mtbx_img2" value="<?php echo esc_url( $em_mtbx_img2 ); ?>" />
-          <input id="img_boton2" type="button" value="Seleccionar imagen" class="img_boton button-secondary"  />
+        <input required id="em_mtbx_img2"  type="hidden" size="40" name="em_mtbx_img2" value="<?php echo esc_url( $em_mtbx_img2 ); ?>" />
+          <input id="img_boton2" type="button" value="Seleccionar imagen" class="img_boton2 button-secondary"  />
+
+          <input type="button" id="image-delete-button2" class="button" value="Borrar Imagen" style="display: none;">
           <br /><br />
-          <?php if($em_mtbx_img2 && $em_mtbx_img2 != '') echo '<img src="' . esc_url( $em_mtbx_img2 ) .'" width="150" height="150" alt="" />'; ?>
+          <?php  
+          if (esc_url( $em_mtbx_img2 )=="") { ?>
+          <img id="image-tag2" src="<?php echo $image; ?>"/><br />
+          <?php }; ?>
+          <?php if($em_mtbx_img2 && $em_mtbx_img2 != '') echo '<img src="' . esc_url( $em_mtbx_img2 ) .'" width="200" height="200"  />'; ?>
           <div class="clear"></div>
           <br />
           
@@ -55,10 +79,15 @@ function em_mtbx_img_function( $post ) { ?>
         $em_mtbx_img3 = get_post_meta( $post->ID, '_em_mtbx_img3', true );
         ?>
         <strong>Foto 3:</strong><br>
-        <input id="em_mtbx_img3" class="em_mtbx_img" type="text" size="40" name="em_mtbx_img3" value="<?php echo esc_url( $em_mtbx_img3 ); ?>" />
-          <input id="img_boton3" type="button" value="Seleccionar imagen" class="img_boton button-secondary"  />
+        <input id="em_mtbx_img3"  type="hidden" size="40" name="em_mtbx_img3" value="<?php echo esc_url( $em_mtbx_img3 ); ?>" />
+          <input id="img_boton3" type="button" value="Seleccionar imagen" class="img_boton3 button-secondary"  />
+          <input type="button" id="image-delete-button3" class="button" value="Borrar Imagen"  style="display: none;">
           <br /><br />
-          <?php if($em_mtbx_img3 && $em_mtbx_img3 != '') echo '<img src="' . esc_url( $em_mtbx_img3 ) .'" width="150" height="150" alt="" />'; ?>
+          <?php  
+          if (esc_url( $em_mtbx_img3 )=="") { ?>
+          <img id="image-tag3"src="<?php echo $image; ?>"/><br />
+          <?php }; ?>
+          <?php if($em_mtbx_img3 && $em_mtbx_img3 != '') echo '<img src="' . esc_url( $em_mtbx_img3 ) .'" width="200" height="200"  />'; ?>
           <div class="clear"></div>
           <br />
 
@@ -66,10 +95,15 @@ function em_mtbx_img_function( $post ) { ?>
         $em_mtbx_img4 = get_post_meta( $post->ID, '_em_mtbx_img4', true );
         ?>
         <strong>Foto 4:</strong><br>
-        <input id="em_mtbx_img4" class="em_mtbx_img" type="text" size="40" name="em_mtbx_img4" value="<?php echo esc_url( $em_mtbx_img4 ); ?>" />
-          <input id="img_boton4" type="button" value="Seleccionar imagen" class="img_boton button-secondary"  />
+        <input id="em_mtbx_img4"  type="hidden" size="40" name="em_mtbx_img4" value="<?php echo esc_url( $em_mtbx_img4 ); ?>" />
+          <input id="img_boton4" type="button" value="Seleccionar imagen" class="img_boton4 button-secondary"  />
+          <input type="button" id="image-delete-button4" class="button" value="Borrar Imagen"  style="display: none;">
           <br /><br />
-          <?php if($em_mtbx_img4 && $em_mtbx_img4 != '') echo '<img src="' . esc_url( $em_mtbx_img4 ) .'" width="150" height="150" alt="" />'; ?>
+          <?php  
+          if (esc_url( $em_mtbx_img4 )=="") { ?>
+          <img id="image-tag4"src="<?php echo $image; ?>" /><br />
+          <?php }; ?>
+          <?php if($em_mtbx_img4 && $em_mtbx_img4 != '') echo '<img src="' . esc_url( $em_mtbx_img4 ) .'" width="200" height="200"  />'; ?>
           <div class="clear"></div>
           <br />     
 
@@ -77,10 +111,15 @@ function em_mtbx_img_function( $post ) { ?>
         $em_mtbx_img5 = get_post_meta( $post->ID, '_em_mtbx_img5', true );
         ?>
         <strong>Foto 5:</strong><br>
-        <input id="em_mtbx_img5" class="em_mtbx_img" type="text" size="40" name="em_mtbx_img5" value="<?php echo esc_url( $em_mtbx_img5 ); ?>" />
-          <input id="img_boton5" type="button" value="Seleccionar imagen" class="img_boton button-secondary"  />
+        <input id="em_mtbx_img5"  type="hidden" size="40" name="em_mtbx_img5" value="<?php echo esc_url( $em_mtbx_img5 ); ?>" />
+          <input id="img_boton5" type="button" value="Seleccionar imagen" class="img_boton5 button-secondary"  />
+          <input type="button" id="image-delete-button5" class="button" value="Borrar Imagen" style="display: none;">
           <br /><br />
-          <?php if($em_mtbx_img5 && $em_mtbx_img5 != '') echo '<img src="' . esc_url( $em_mtbx_img5 ) .'" width="150" height="150" alt="" />'; ?>
+         <?php  
+          if (esc_url( $em_mtbx_img5 )=="") { ?>
+          <img id="image-tag5" src="<?php echo $image; ?>"/><br />
+          <?php }; ?>
+          <?php if($em_mtbx_img5 && $em_mtbx_img5 != '') echo '<img src="' . esc_url( $em_mtbx_img5 ) .'" width="200" height="200"  />'; ?>
           <div class="clear"></div>
           <br />
           <?php 
@@ -93,6 +132,7 @@ add_action('admin_print_scripts-post-new.php', 'em_mtbx_img_admin_scripts');
 function em_mtbx_img_admin_scripts() {
         wp_enqueue_script( 'em-image-upload', plugins_url( '/imagenes-meta-box/imagenes-meta-box.js' ), array( 'jquery','media-upload','thickbox' ) );
 }
+
  
 //usando thickbox para subir la imagen al crear o editar una entrada
 add_action('admin_print_styles-post.php', 'em_mtbx_img_admin_styles');
@@ -139,3 +179,4 @@ function em_mtbx_img_save_meta( $post_id ) {
 
 
 ?>
+
